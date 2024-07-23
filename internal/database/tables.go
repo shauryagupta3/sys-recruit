5efbdf5710f10c631bd817a5f8231ced23c5bfd1
Shaurya Gupta <119103602+shauryagupta3@users.noreply.github.com>
2024-07-23 19:34:15 +0530

    added create cmd for tables

--- internal/database/tables.go
+++ internal/database/tables.go
@@ -7,7 +7,7 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-const createTableSQL = `
+const createUsersTable = `
 CREATE TABLE IF NOT EXISTS users (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(50) NOT NULL,
@@ -18,11 +18,49 @@ CREATE TABLE IF NOT EXISTS users (
 	created_at TIMESTAMP DEFAULT NOW()
 );
 `
+const createProfilesTable = `
+CREATE TABLE IF NOT EXISTS profiles (
+    user_id INTEGER PRIMARY KEY,
+    resume_file_address VARCHAR(255),
+    skills TEXT,
+    education TEXT,
+    experience TEXT,
+    name VARCHAR(100),
+    email VARCHAR(100),
+    phone VARCHAR(20),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE SET NULL
+);`
+
+const createJobsTable = `
+CREATE TABLE IF NOT EXISTS jobs (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    posted_on TIMESTAMP DEFAULT NOW(),
+    total_applications INTEGER DEFAULT 0,
+    company_name VARCHAR(255) NOT NULL,
+    posted_by_id INTEGER NOT NULL,
+    FOREIGN KEY (posted_by_id) REFERENCES users(id) ON DELETE SET NULL
+);`
+
+const createJobProfileTable = `
+CREATE TABLE IF NOT EXISTS job_profiles (
+    job_id INTEGER NOT NULL,
+    profile_id INTEGER NOT NULL,
+    PRIMARY KEY (job_id, profile_id),
+    FOREIGN KEY (job_id) REFERENCES jobs(id) ON DELETE CASCADE,
+    FOREIGN KEY (profile_id) REFERENCES profiles(user_id) ON DELETE CASCADE
+);`
 
 func CreateTables(ctx context.Context, db *pgxpool.Pool) error {
-	_, err := db.Exec(ctx, createTableSQL)
-	if err != nil {
-		return fmt.Errorf("failed to execute SQL statement: %w", err)
+	tables := []string{createUsersTable, createProfilesTable, createJobsTable, createJobProfileTable}
+
+	for _, table := range tables {
+		_, err := db.Exec(ctx, table)
+		if err != nil {
+			return fmt.Errorf("failed to execute SQL statement: %w", err)
+		}
+
 	}
 
 	return nil
